ec30df52d5a0a5b3214adfb43d6301e6d6273b0e
ssomethingg00 <gameinstall2025@gmail.com>
2026-06-05 01:24:06 +0530

    Register UpdatePlugin in MainActivity

--- android/app/src/main/java/com/webgames/gamehub/MainActivity.java
+++ android/app/src/main/java/com/webgames/gamehub/MainActivity.java
@@ -1,5 +1,12 @@
 package com.webgames.gamehub;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(UpdatePlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
